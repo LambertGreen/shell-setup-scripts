67692420be1445405bdfd24ee7781581a4299e9d
Lambert Green <lgreen@tableau.com>
2018-06-25 15:25:03 -0700

    Add call to ConEmu statup script; Add some useful aliases.

--- cmd/startup.cmd
+++ cmd/startup.cmd
@@ -1,8 +1,22 @@
 @echo off
 
+:: ------------------------------------------------
+:: Run ConEmu script
+:: ------------------------------------------------
+call "%ConEmuBaseDir%\CmdInit.cmd"
+
+:: ------------------------------------------------
+:: aliases
+:: ------------------------------------------------
 doskey ls=ls --color
 doskey dev=pushd %userprofile%\dev
 doskey home=pushd %userprofile%
+
+doskey  pycharm=pycharm64.exe $*
+doskey  webstorm=webstorm64.exe $*
+doskey  idea=idea64.exe $*
+doskey  clion=clion64.exe $*
+doskey  n=notepad++.exe $*
 
 :: ------------------------------------------------
 :: Set a nice prompt
